c87903eb51d75e15c7a96af76dac9004b4158b72
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2023-03-12 23:42:09 +0600

    cart dialog box component

---
+++ src/app/shared/dialog/cart-dialog/cart-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-dialog',
+  templateUrl: './cart-dialog.component.html',
+  styleUrls: ['./cart-dialog.component.scss']
+})
+export class CartDialogComponent {
+
+}
