a50bbb495744489096f0c37b8e797ff5efb3ab50
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2023-08-05 13:33:40 +0600

    changed default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/new', pathMatch: 'full' },
+    { path: '', redirectTo: '/products', pathMatch: 'full' },
 
     {
         path: '',
